ac5b5dfa16958b7b6ea6a6ed6d7d6871694066a9
visal rengan <visalrengan18@gmail.com>
2026-01-18 18:34:55 +0530

    Footer Added

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,72 @@
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                {/* Brand Section */}
+                <div className="footer-brand">
+                    <Link to="/" className="footer-logo">
+                        <span className="footer-logo-text">JobSwipe</span>
+                    </Link>
+                    <p className="footer-tagline">
+                        Connecting talent with opportunity through intelligent matching.
+                    </p>
+                </div>
+
+                {/* Links Section */}
+                <div className="footer-links">
+                    <div className="footer-links-column">
+                        <h4 className="footer-links-title">For Job Seekers</h4>
+                        <Link to="/seeker/jobs" className="footer-link">Find Jobs</Link>
+                        <Link to="/seeker/companies" className="footer-link">Browse Companies</Link>
+                        <Link to="/seeker/applications" className="footer-link">My Applications</Link>
+                    </div>
+
+                    <div className="footer-links-column">
+                        <h4 className="footer-links-title">For Employers</h4>
+                        <Link to="/company/post-job" className="footer-link">Post a Job</Link>
+                        <Link to="/company/jobs" className="footer-link">Manage Jobs</Link>
+                        <Link to="/company/matches" className="footer-link">View Matches</Link>
+                    </div>
+
+                    <div className="footer-links-column">
+                        <h4 className="footer-links-title">Platform</h4>
+                        <Link to="/search" className="footer-link">Search</Link>
+                        <Link to="/network" className="footer-link">Network</Link>
+                        <Link to="/chats" className="footer-link">Messages</Link>
+                    </div>
+                </div>
+
+                {/* Bottom Bar */}
+                <div className="footer-bottom">
+                    <p className="footer-copyright">
+                        © {currentYear} JobSwipe. All rights reserved.
+                    </p>
+                    <div className="footer-social">
+                        <a href="#" className="footer-social-link" aria-label="LinkedIn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                            </svg>
+                        </a>
+                        <a href="#" className="footer-social-link" aria-label="Twitter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                            </svg>
+                        </a>
+                        <a href="#" className="footer-social-link" aria-label="Instagram">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
